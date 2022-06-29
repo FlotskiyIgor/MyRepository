8a1cd8a6e2ecf0854f8588dab506009a2bcb2957
Igor <108419274+FlotskiyIgor@users.noreply.github.com>
2022-06-29 19:18:39 +0300

    Мат Логика
    Оконное приложение на C# Windows Forms для реализации логических операций с  обработками исключений

---
+++ MatLog/MatLog/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace MatLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 616);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(121, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Загрузим фотографию для фона?)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(711, 24);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(622, 574);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(345, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "P or Q or R and S and not P";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "P";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Q";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(235, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "R";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(295, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "S";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 22);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(166, 169);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 22);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(225, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 22);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(285, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 22);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(348, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ответ: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(353, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 44);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Посчитаем?)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(121, 90);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 36);
+            this.listBox1.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(57, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 59);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Ответ первого действия";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(204, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(230, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "После первого действия получим";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(57, 273);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 59);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Ответ второго действия";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(204, 294);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(229, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "После второго действия получим";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(57, 344);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(126, 59);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Ответ третьего действия";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(204, 365);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(240, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "После третьего действия получим ";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(57, 420);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(126, 59);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Ответ четвертого действия";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(204, 441);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(252, 17);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "После четвертого действия получим";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(357, 109);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(163, 17);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Логическое выражение";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(537, 24);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(145, 44);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "Очистка";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1345, 759);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ MatLog/MatLog/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false; // изначально блокируем кнопки 2-6, чтобы без введения значений мы не могли нажать на кнопку получения ответа
+            button3.Enabled = false; // для 3 кнопки
+            button4.Enabled = false; // для 4 кнопки
+            button5.Enabled = false; // для 5 кнопки
+            button6.Enabled = false; // для 6 кнопки
+            button7.Enabled = false; // для 7 кнопки
+        }
+
+        int a;// задаем глобальную переменную для а
+        int b;// задаем глобальную переменную для b
+        int ab; // задаем глобальную переменную для аb
+        int abc; // задаем глобальную переменную для аbc
+        int abcd; // задаем глобальную переменную для аbcd
+        int abcdf = 0; // задаем глобальную переменную для аbcde
+
+        private void button1_Click(object sender, EventArgs e) // метод для баловства, загрузил фотку для фона прикол хаха
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*JPG;*.GIF;*.PNG| All files (*.*)|*.*;"; // позволяем ввести в картинку прикол заданные типы файлов
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName); // позволяет внести саму картинку
+                }
+                catch
+                {
+                    MessageBox.Show("невозмжно открыть выбранный файл", "ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); // выдает ошибку если была внесена вместо заданных типов файлов иной тип
+
+                }
+            }
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) // метод для текстбокса 1  чтобы в окно можно было вводить либо 1 либо 0
+        {
+
+
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 50) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e) // метод для текстбокса 2 чтобы в окно можно было вводить либо 1 либо 0
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 50) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e) // метод для текстбокса 3 чтобы в окно можно было вводить либо 1 либо 0
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 50) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e) // метод для текстбокса 4 чтобы в окно можно было вводить либо 1 либо 0
+        {
+            char number = e.KeyChar;
+            if ((e.KeyChar <= 47 || e.KeyChar >= 50) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) // метод для текстбокса1 который не позволит нам ввести число наподобие 11 00 01 1110 и т.д.
+        {
+            //Проверка на пустоту, равность 0 и исключение чисел > 9
+            if (textBox1.Text == ""
+            || textBox1.Text.Count() > 1
+            || textBox2.Text == ""
+            || textBox2.Text.Count() > 1
+            || textBox3.Text == ""
+            || textBox3.Text.Count() > 1
+            || textBox4.Text == ""
+            || textBox4.Text.Count() > 1)
+            {
+                button2.Enabled = false;
+                listBox1.Items.Clear();
+                
+            }
+            //Проверка ввода чисел до 10, исключая 0
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Ввод корректный.");
+                button2.Enabled = true;
+            }
+            if ((textBox1.Text == "") || (textBox2.Text == "")|| (textBox1.Text.Count() > 1) || textBox2.Text.Count() > 1) // позволяет нажать на получение 1 ответа после введение P и Q
+            {
+                button3.Enabled = false;
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Некорректный ввод.");
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Ввод корректный.");
+                button3.Enabled = true;
+                button7.Enabled = true;
+            }
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == "") || (textBox1.Text.Count() > 1) || textBox2.Text.Count() > 1 || textBox3.Text.Count()>1) // позволяет нажать на получение 2 ответа после введения  R 
+            {
+                button4.Enabled = false;
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Некорректный ввод.");
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Ввод корректный.");
+                button4.Enabled = true;
+            }
+            if ((textBox1.Text == "") || (textBox2.Text == "") || (textBox3.Text == "") || (textBox4.Text=="") || (textBox1.Text.Count() > 1) || textBox2.Text.Count() > 1 || textBox3.Text.Count() > 1 || textBox4.Text.Count()>1) // позволяет нажать на получение ответа 3 и ответа 4 после введения S
+            {
+                button5.Enabled = false;
+                button6.Enabled = false;
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Некорректный ввод. ");
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Ввод корректный.");
+                button5.Enabled = true;
+                button6.Enabled = true;
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) // метод для пошагового вычисления (действие 1)
+        {
+             a = Convert.ToInt32(textBox1.Text);
+             b = Convert.ToInt32(textBox2.Text);
+             ab = 0;
+            if ((a==0) && (b==0))
+            {
+                ab = 0;
+                
+            }
+            if ((a == 0) && (b == 1))
+            {
+                ab = 1;
+
+            }
+            if ((a == 1) && (b == 0))
+            {
+                ab = 1;
+
+            }
+            if ((a == 1) && (b == 1))
+            {
+                ab = 1;
+
+            }
+
+            label7.Text = "После первого действия получим " + ab;
+        }
+
+        private void button4_Click(object sender, EventArgs e) //  метод для пошагового вычисления (действие 2)
+        {
+            abc = 0;
+            int c = Convert.ToInt32(textBox3.Text);
+            if ((ab == 0) && (c == 0))
+            {
+                abc = 0;
+
+            }
+            if ((ab == 0) && (c == 1))
+            {
+                abc = 1;
+
+            }
+            if ((ab == 1) && (c == 0))
+            {
+                abc = 1;
+
+            }
+            if ((ab == 1) && (c == 1))
+            {
+                abc = 1;
+
+            }
+            label8.Text = "После второго действия получим " + abc;
+        }
+
+        private void button5_Click(object sender, EventArgs e) // метод для пошагового вычисления (действие 3)
+        {
+            abcd = 0;
+            int d = Convert.ToInt32(textBox4.Text);
+            if ((abc == 0) && (d == 0))
+            {
+                abcd = 0;
+
+            }
+            if ((abc == 0) && (d == 1))
+            {
+                abcd = 0;
+
+            }
+            if ((abc == 1) && (d == 0))
+            {
+                abcd = 0;
+
+            }
+            if ((abc == 1) && (d == 1))
+            {
+                abcd = 1;
+
+            }
+            label9.Text = "После третьего действия получим " + abcd;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Сам метод для получения конечного ответа ( действие 4)
+        {
+            int f = 0;
+            abcdf = 0;
+            if (a == 0) // Условия для получения not P
+            {
+                f = 1;
+            }
+            if (a == 1)
+            {
+                f = 0;
+            }
+
+            if ((abcd == 0) && (f == 0)) // Логическое выражение для 4 действия
+            {
+                abcdf = 0;
+
+            }
+            if ((abcd == 0) && (f == 1))
+            {
+                abcdf = 0;
+
+            }
+            if ((abcd == 1) && (f == 0))
+            {
+                abcdf = 0;
+
+            }
+            if ((abcd == 1) && (f == 1))
+            {
+                abcdf = 1;
+
+            }
+            label10.Text = "После четвертого действия получим " + abcdf;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // выводит полный ответ
+        {
+
+            label6.Text = "Ответ: " + abcdf;
+        }
+
+        private void button7_Click(object sender, EventArgs e) // максимально ленивый метод очистки ответов по кнопке
+        {
+            label6.Text = "Ответ: ";
+            label7.Text = "После первого действия получим ";
+            label8.Text = "После второго действия получим ";
+            label9.Text = "После третьего действия получим ";
+            label10.Text = "После четвертого действия получим ";
+        }
+    }
+}
