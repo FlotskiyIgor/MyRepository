1191ea16058b1b6bf762643ed70d2fec38ed860b
Igor <108419274+FlotskiyIgor@users.noreply.github.com>
2022-06-29 19:23:26 +0300

    Маленький проект C# Windows Forms
    Вычислительные и логические операции

---
+++ IgoryanFloat/IgoryanFloat/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace IgoryanFloat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите n=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(490, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Введите k=";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(368, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(49, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(593, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 510);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Получаем ответ:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 101);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Перестановка без повторений";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 101);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Размещение без повторений";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 101);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Сочетание с повторениями";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(468, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(194, 101);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Упорядоченная урновая модель";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(468, 236);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 101);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Размещение с повторениями";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(468, 353);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(194, 101);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Сочетание без повторения";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 609);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ IgoryanFloat/IgoryanFloat/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IgoryanFloat
+{
+    public partial class Form1 : Form
+    {
+        
+        int n;
+        int k;
+        int rezult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Factorial(int n)
+        {
+            if (n <= 0)
+            {
+                MessageBox.Show("Факториал меньше или равен нулю");
+                return 0;
+            }
+            else
+            {
+                int factorial = 1;
+                for (int i = 1; i <= n; i++)
+                    factorial = factorial * i;
+                return factorial;
+            }
+
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                rezult = Factorial(n);
+
+                label3.Text = "Получаем ответ: P(n) = n! = " + rezult;
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели данные некорректно, пожалуйста, введите правильно");
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                k = Convert.ToInt32(textBox2.Text);
+                if (k >= n) MessageBox.Show("Делить на ноль нельзя.Значение k должно быть меньше значения n");
+                else
+                {
+                    rezult = Factorial(n) / Factorial(n - k);
+                    label3.Text = "Получаем ответ:  n!/(n-k)! = " + rezult;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели данные некорректно, пожалуйста, введите правильно");
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                k = Convert.ToInt32(textBox2.Text);
+                if (k >= n) MessageBox.Show("Делить на ноль нельзя.Значение k должно быть меньше значения n");
+                else
+                {
+                    rezult = Factorial(n + k - 1) / (Factorial(k) * Factorial(n - 1));
+                    label3.Text = "Получаем ответ: С (k n) = С (k n+k-1) = (n+k-1)!/(k!(n-1)!) = " + rezult;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели данные некорректно, пожалуйста, введите правильно");
+            }
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                k = Convert.ToInt32(textBox2.Text);
+                if (k >= n) MessageBox.Show("Делить на ноль нельзя.Значение k должно быть меньше значения n");
+                else
+                {
+                    rezult = Factorial(n) / Factorial(n - k);
+                    label3.Text = "Получаем ответ: А (k n) = n!/(n-k)! = " + rezult;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели данные некорректно, пожалуйста, введите правильно");
+            }
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double n = Convert.ToDouble(textBox1.Text);
+                double k = Convert.ToDouble(textBox2.Text);
+                double rezult = Math.Pow(n, k);
+                label3.Text = "Получает ответ: A (k n) = n^k = " + rezult;
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели данные некорректно, пожалуйста, введите правильно");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = Convert.ToInt32(textBox1.Text);
+                k = Convert.ToInt32(textBox2.Text);
+                if (k >= n) MessageBox.Show("Делить на ноль нельзя.Значение k должно быть меньше значения n");
+                else
+                {
+                    rezult = Factorial(n) / (Factorial(k) * Factorial(n - k));
+                    label3.Text = "Получаем ответ: С (k n) = n!/((n-k)!k!) = " + rezult;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Вы ввели данные некорректно, пожалуйста, введите правильно");
+            }
+        }
+    }
+   
+
+       
+    
+}
